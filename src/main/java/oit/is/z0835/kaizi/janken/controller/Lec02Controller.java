8502b9dfaa158cefde3778b61b19c1500662df3d
shunsh65 <e1b18067@oit.ac.jp>
2020-10-27 12:30:27 +0900

    セキュリティ認証処理

--- src/main/java/oit/is/z0835/kaizi/janken/controller/Lec02Controller.java
+++ src/main/java/oit/is/z0835/kaizi/janken/controller/Lec02Controller.java
@@ -1,16 +1,34 @@
 package oit.is.z0835.kaizi.janken.controller;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import oit.is.z0835.kaizi.janken.model.Janken;
-
+import oit.is.z0835.kaizi.janken.model.Entry;
 @Controller
 public class Lec02Controller {
+
+  @Autowired
+  private Entry room;
+
+
+  @GetMapping("lec02")
+  public String lec02(ModelMap model, Principal prin) {
+    String loginUser = prin.getName(); // ログインユーザ情報
+    this.room.addUser(loginUser);
+    model.addAttribute("room", this.room);
+    return "lec02.html";
+  }
+
+
   @PostMapping("/lec02")
   public String lec02(@RequestParam String name, ModelMap model) {
     String username = name;
